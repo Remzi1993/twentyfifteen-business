9f9f9ae83e246729ceb5425d0f65c0b8f521cc62
Remzi <ict@remzi.info>
2019-12-02 01:18:40 +0100

    Not needed anymore, because of jQuery Manager

--- functions.php
+++ functions.php
@@ -17,15 +17,3 @@ add_filter('stylesheet_uri', 'use_parent_theme_stylesheet');
 
 // Enqueue this theme's scripts and styles (after parent theme)
 add_action('wp_enqueue_scripts', 'my_theme_styles', 20);
-
-
-// Disable jQuery and jQuery Migrate in WordPress frontend.
-add_filter( 'wp_enqueue_scripts', 'change_default_jquery', PHP_INT_MAX );
-
-function change_default_jquery( ){
-    wp_dequeue_script( 'jquery');
-    wp_deregister_script( 'jquery');
-    wp_dequeue_script( 'jquery-migrate');
-    wp_deregister_script( 'jquery-migrate');
-    wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', array(), null, true);
-}
